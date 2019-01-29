9678c07d1abe412aa1ba52e3d23702bf670fca8a
rafamolino <molinorafael99@gmail.com>
2019-01-29 10:18:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pc/actividad1tema7/DB_SQLite.java
@@ -0,0 +1,31 @@
+package com.example.pc.actividad1tema7;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DB_SQLite extends SQLiteOpenHelper
+{
+    private static final String DATABASE_NAME = "tienda";
+    private static final int DATABASE_VERSION = 1;
+    private static final String SQL_CREATE_ENTRIES = "CREATE TABLE PRODUCTO( _id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, cantidad INTEGER, seccion TEXT)";
+    private static final String SQL_DELETE_ENTRIES = "DROP TABLE PRODUCTO";
+    public DB_SQLite(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+    db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+    db.execSQL(SQL_DELETE_ENTRIES);
+    
+    }
+
+}
