e80e21ff221c5fb41774cebbfbdd4289c991ba25
rafamolino <Xxracso40xX@gmail.com>
2019-01-30 09:40:24 +0100

    buscar opción

--- app/src/main/java/com/example/pc/actividad1tema7/MainActivity.java
+++ app/src/main/java/com/example/pc/actividad1tema7/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pc.actividad1tema7;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,11 +12,15 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.sql.ResultSet;
+
 public class MainActivity extends AppCompatActivity {
     private EditText txtNombre;
     private EditText txtCantidad;
     private Spinner lstSeccion;
-
+    String strNombre;
+    String strCantidad;
+    String strSeccion;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -39,18 +44,24 @@ public class MainActivity extends AppCompatActivity {
 
     public void insertarProducto(View view)
     {
-    String strNombre = txtNombre.getText().toString();
-    String strCantidad = txtCantidad.getText().toString();
-    String strSeccion = lstSeccion.getSelectedItem().toString();
-    if(strNombre.equals("") || strCantidad.equals(""))
-    {
-        mostrarMensaje("Todos los campos son obligatorios");
+        strNombre = txtNombre.getText().toString();
+        strCantidad = txtCantidad.getText().toString();
+        strSeccion = lstSeccion.getSelectedItem().toString();
+        if(strNombre.equals("") || strCantidad.equals(""))
+        {
+            mostrarMensaje("Todos los campos son obligatorios");
 
-    }
-    else
-    {
-
-    }
+        }
+        else
+        {
+            String sql = "INSERT INTO PRODUCTO (nombre, cantidad, seccion) VALUES ('" + strNombre + "'," + Integer.parseInt(strCantidad) + ",'" + strSeccion + "' )";
+            DB_SQLite db = new DB_SQLite(this);
+            SQLiteDatabase conn = db.getWritableDatabase();
+            conn.execSQL(sql);
+            conn.close();
+            mostrarMensaje("El producto" + strNombre + "ha sido insertado");
+            limpiarCuadros();
+        }
     }
 
     public void eliminarProducto(View view)
@@ -61,16 +72,51 @@ public class MainActivity extends AppCompatActivity {
     {
 
     }
-
     public void buscarProducto(View view)
     {
+        strNombre = txtNombre.getText().toString();
+        if(strNombre.equals("") )
+        {
+        mostrarMensaje("Debe indicar el producto a monstrar");
+        }
+        else
+        {
+            String sql1 = "SELECT _id, nombre, cantidad, seccion FROM PRODUCTO WHERE nombre LIKE '" + strNombre + "'";
+            DB_SQLite db = new DB_SQLite(this);
+            SQLiteDatabase conn = db.getReadableDatabase();
+            Cursor cursor = conn.rawQuery(sql1,null);
+            if(cursor.getCount()==0)
+            {
+                mostrarMensaje("El producto" + strNombre + "no existe");
+            }
+            else
+            {
+                cursor.moveToFirst();
+                Long dataIdProducto = cursor.getLong(cursor.getColumnIndex("_id"));
+                String dataNombreProducto =cursor.getString(cursor.getColumnIndex("nombre"));
+                Integer dataCantidadProducto =cursor.getInt(cursor.getColumnIndex("cantidad"));
+                String dataSeccionProducto =cursor.getString(cursor.getColumnIndex("seccion"));
+                mostrarMensaje("El id es" + dataIdProducto + ", el nombre es " + dataNombreProducto + ", la cantidad es " + dataCantidadProducto + " y la seccion es " + dataSeccionProducto);
+
+            }
+            cursor.close();
+            conn.close();
+        }
+
 
     }
+    public void listarProducto(View view)
+    {
 
-    public void listarProducto(View view) {
     }
     private void mostrarMensaje(String msg)
     {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT);
     }
+    private void limpiarCuadros()
+    {
+        txtNombre.setText("");
+        txtCantidad.setText("");
+    }
 }
+
