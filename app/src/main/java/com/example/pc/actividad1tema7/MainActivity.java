4e1af32e2014afdac49ba5665c35180b8638e827
rafamolino <molinorafael99@gmail.com>
2019-01-30 08:26:16 +0100

    Rellenar spinner

--- app/src/main/java/com/example/pc/actividad1tema7/MainActivity.java
+++ app/src/main/java/com/example/pc/actividad1tema7/MainActivity.java
@@ -4,21 +4,36 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText txtNombre;
+    private EditText txtCantidad;
+    private Spinner lstSeccion;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DB_SQLite db = new DB_SQLite(this);
+        txtNombre = findViewById(R.id.editText);
+        txtCantidad = findViewById(R.id.editText2);
+        lstSeccion = findViewById(R.id.spinner);
+
+        String[] secciones = {"Monitor", "DiscoDuro", "Memoria", "Teclado", "Ratón", "Impresora"};
+        lstSeccion.setAdapter(new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, secciones));
+       /* DB_SQLite db = new DB_SQLite(this);
         SQLiteDatabase conn = db.getWritableDatabase();
         conn.execSQL("INSERT INTO PRODUCTO (nombre, cantidad, seccion) VALUES ('uno',124,'monitor1')");
         conn.execSQL("INSERT INTO PRODUCTO (nombre, cantidad, seccion) VALUES ('dos',125,'monitor2')");
         conn.execSQL("INSERT INTO PRODUCTO (nombre, cantidad, seccion) VALUES ('tres',126,'monitor3')");
         conn.execSQL("INSERT INTO PRODUCTO (nombre, cantidad, seccion) VALUES ('cuatro',127,'monitor4')");
         conn.close();
+        */
     }
 
     public void insertarProducto(View view)
